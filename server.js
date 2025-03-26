5a0b557d3add6d2746a0a044cc3ce537138a483f
Tomosiako <bulimotombrian001@gmail.com>
2025-03-26 18:46:58 +0300

    added a post request

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser')
 
 
 //app.set('view engine','ejs');
-//app.set('view engine','hbs');
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//app.use(bodyParser.json());
 let names=['alii','elishishe'];
 
 app.get('/',(req,res)=>{
-    const name = "cisco"
+    const names = "cisco"
     res.render('./index',{names})
+})
+
+app.post('/register',(req,res)=>{
+    const name  = req.body.name;
+    const email  = req.body.email;
+    const admnNo  = req.body.admnNo;
+
+    
+    var details = [name,email,admnNo];
+
+    console.log(details);
+    
+    
+
 })
 
 
